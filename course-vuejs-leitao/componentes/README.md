3085e1f9d696b7333a7e8a89d6b21fcc6bd89799
ailtonbsj <ailton.ifce@gmail.com>
2020-10-17 21:24:54 -0300

    creating components

---
+++ course-vuejs-leitao/componentes/README.md
@@ -0,0 +1,10 @@
+# History of commands typed
+
+```bash
+vue create componente-intro
+# Default (Vue 2) preset used
+
+cd componente-intro
+
+npm run serve
+```
---
+++ course-vuejs-leitao/componentes/componente-intro/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Contadores</h1>
+    <app-contador />
+    <app-contador />
+    <app-contador />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0,
+    }
+  },
+};
+</script>
+
+<style>
+</style>
