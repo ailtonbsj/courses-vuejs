3533c34effc0231e764793a5db341079713d22ad
ailtonbsj <ailton.ifce@gmail.com>
2020-10-19 22:26:44 -0300

    keeping alive components

--- course-vuejs-leitao/componentes/componentes-avancado/src/components/Citacao.vue
+++ course-vuejs-leitao/componentes/componentes-avancado/src/components/Citacao.vue
@@ -1,20 +1,33 @@
 <template>
-    <div class="citacao">
-        <slot name="autor"></slot>
-        <slot name="texto"></slot>
-        <slot name="fonte"></slot>
-    </div>
+  <div class="citacao">
+    <slot name="autor"></slot>
+    <slot name="texto"></slot>
+    <slot name="fonte"></slot>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  created() {
+      console.log('Created citacao')
+  },
+  detroyed() {
+      console.log('Destroyed citacao')
+  },
+  activated() {
+      console.log('Activated citacao')
+  },
+  deactivated() {
+      console.log('Deactivated citacao')
+  }
+}
 </script>
 
 <style scoped>
-    .citacao {
-        border: 1px solid #DDD;
-        background-color: rgba(0, 0, 0, .1);
-        padding: 25px;
-        max-width: 450px;
-    }
+.citacao {
+  border: 1px solid #ddd;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 25px;
+  max-width: 450px;
+}
 </style>
