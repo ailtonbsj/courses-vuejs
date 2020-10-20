935a9604f66e9dd6b52c1367dbbd040a94ffa229
ailtonbsj <ailton.ifce@gmail.com>
2020-10-20 00:07:28 -0300

    slots challenge completed

---
+++ course-vuejs-leitao/componentes/comp-avancado-desafio/src/components/Verde.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="caixa verde">
+        <slot name="Verde" />
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ course-vuejs-leitao/componentes/comp-avancado-desafio/src/components/Vermelho.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="caixa vermelho">
+        <slot name="Vermelho" />
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
