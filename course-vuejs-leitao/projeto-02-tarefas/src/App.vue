2593b9847ea4807fa7e6b13eba0e425d9b1bcb16
ailtonbsj <ailton.ifce@gmail.com>
2020-10-21 17:02:16 -0300

    started project two

---
+++ course-vuejs-leitao/projeto-02-tarefas/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <h1>Tarefas</h1>
+    <NewTask @taskAdded="addTask" />
+    <TaskGrid :tasks="tasks" />
+  </div>
+</template>
+
+<script>
+import TaskGrid from "./components/TaskGrid.vue";
+import NewTask from "./components/NewTask.vue";
+
+export default {
+  components: {
+    TaskGrid,
+    NewTask,
+  },
+  data() {
+    return {
+      tasks: [
+        { name: "Lavar a louça", pending: false },
+        { name: "Comprar blusa", pending: true },
+      ],
+    };
+  },
+  methods: {
+    addTask(task) {
+      const reallyNew = !this.tasks.some((t) => t.name === task.name);
+      reallyNew &&
+        this.tasks.push({
+          name: task.name,
+          pending: task.pending || true,
+        })
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: "Lato", sans-serif;
+  background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+  color: #fff;
+}
+
+#app {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+#app h1 {
+  margin-bottom: 5px;
+  font-weight: 300;
+  font-size: 3rem;
+}
+</style>
