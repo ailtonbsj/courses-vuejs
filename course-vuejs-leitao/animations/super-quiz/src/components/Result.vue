48353982690563456911a27af029e7a41f255fd7
ailtonbsj <ailton.ifce@gmail.com>
2020-10-26 09:38:16 -0300

    Finished module animations

--- course-vuejs-leitao/animations/super-quiz/src/components/Result.vue
+++ course-vuejs-leitao/animations/super-quiz/src/components/Result.vue
@@ -1,12 +1,19 @@
 <template>
-    <div class="result">
-        
+    <div class="result" :class="{wrong:!result}">
+        <span v-if="result">Resposta Certa! :)</span>
+        <span v-else>Resposta Errada! :(</span>
+        <button @click="$emit('confirmed')">Proxima Pergunta</button>
     </div>
 </template>
 
 <script>
 export default {
-    
+    props: {
+        result: {
+            type: Boolean,
+            required: true
+        }
+    }
 }
 </script>
 
