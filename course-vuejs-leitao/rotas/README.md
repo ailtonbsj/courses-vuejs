7694687c48aec3c17fe41ee76773b7c7e660936b
ailtonbsj <ailton.ifce@gmail.com>
2020-10-30 16:55:03 -0300

    Using routes

---
+++ course-vuejs-leitao/rotas/README.md
@@ -0,0 +1,5 @@
+# Commands used
+
+```bash
+npm i vue-router@3.0.2 -E
+```
---
+++ course-vuejs-leitao/rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="usuario">
+    <h2>Usuários</h2>
+    <hr />
+    <p><strong>Código: </strong> {{ idx }}</p>
+    <button sucesso @click="irParaInicio">Voltar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    irParaInicio() {
+      this.$router.push("/");
+    },
+  },
+  data() {
+    return {
+      idx: this.$route.params.id,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
