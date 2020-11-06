ecbfa8a6a0ae97d1e985fd011c1385a98f4d3d5c
ailtonbsj <ailton.ifce@gmail.com>
2020-11-05 23:21:22 -0300

    adding confs for use app in subfolders

--- course-vuejs-leitao/projeto-03-trader/stock-trader/src/router.js
+++ course-vuejs-leitao/projeto-03-trader/stock-trader/src/router.js
@@ -5,10 +5,13 @@ import Home from './components/Home'
 import Portfolio from './components/portfolio/Portfolio'
 import Stocks from './components/stocks/Stocks'
 
+import { publicPath } from '../vue.config'
+
 Vue.use(Router)
 
 export default new Router({
     mode: 'history',
+    base: publicPath,
     routes: [
         { path: '/', component: Home },
         { path: '/portfolio', component: Portfolio },
