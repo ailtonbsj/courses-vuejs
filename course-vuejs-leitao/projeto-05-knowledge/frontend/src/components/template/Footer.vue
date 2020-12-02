e67c16e839704451cfd5131010e303af220b64c2
ailtonbsj <ailton.ifce@gmail.com>
2020-12-02 00:28:01 -0300

    started frontend of project knowledge

---
+++ course-vuejs-leitao/projeto-05-knowledge/frontend/src/components/template/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="footer">
+    <span>Copyright <strong>Cod3r</strong> &copy; 2015-2018.</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style>
+.footer {
+  grid-area: footer;
+  background: #ddd;
+  color: #333;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding-right: 10px;
+}
+</style>
--- course-vuejs-leitao/projeto-05-knowledge/frontend/src/main.js
+++ course-vuejs-leitao/projeto-05-knowledge/frontend/src/main.js
@@ -1,9 +1,12 @@
+import 'font-awesome/css/font-awesome.min.css'
 import Vue from 'vue'
-
 import App from './App'
+import './config/bootstrap'
+import store from './config/store'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app')
